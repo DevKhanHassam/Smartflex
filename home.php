3efa526ad085fd885b97df4bd0da105a68303dfa
Hassam Khan <68502565+DevKhanHassam@users.noreply.github.com>
2023-07-31 22:49:13 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,88 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php 
+$email = $_SESSION['email'];
+$password = $_SESSION['password'];
+if($email != false && $password != false){
+    $sql = "SELECT * FROM usertable WHERE email = '$email'";
+    $run_Sql = mysqli_query($con, $sql);
+    if($run_Sql){
+        $fetch_info = mysqli_fetch_assoc($run_Sql);
+        $status = $fetch_info['status'];
+        $code = $fetch_info['code'];
+        if($status == "verified"){
+            if($code != 0){
+                header('Location: reset-code.php');
+            }
+        }else{
+            header('Location: user-otp.php');
+        }
+    }
+}else{
+    header('Location: login-user.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $fetch_info['name'] ?> | Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+    nav{
+        padding-left: 100px!important;
+        padding-right: 100px!important;
+        background: #000000;
+        font-family: 'Oswald', sans-serif;
+        font-size: 30px;
+	    text-decoration: none;
+	    color:#ffffff;
+	    font-weight: 700;
+    } 
+    nav a.navbar-brand{
+        /* color:white;
+        font-size: 30px!important;
+        font-weight: 500; */
+
+        font-size: 30px;
+	    text-decoration: none;
+	    color:#ffffff;
+	    font-weight: 700;
+    }
+
+    button{
+        background:grey;
+    }
+    button a{
+        color: #c11325;
+        font-weight: 500;
+    }
+    button a:hover{
+        color:black
+        text-decoration: none;
+    }
+
+    span{
+        color: #c11325;
+    }
+    h1{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        text-align: center;
+        transform: translate(-50%, -50%);
+        font-size: 50px;
+        font-weight: 600;
+    }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+    <a class="navbar-brand" href="#">Smart <span>Flex</span></a>
+    <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
+    </nav>
+    <h1>Welcome <?php echo $fetch_info['name'] ?></h1>
+    
+</body>
+</html>
