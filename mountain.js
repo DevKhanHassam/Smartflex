6393ce8f57794dcaaa32011020c5c2a3c0a4d284
Hassam Khan <68502565+DevKhanHassam@users.noreply.github.com>
2023-08-18 18:26:19 +0200

    Add files via upload

---
+++ mountain.js
@@ -0,0 +1,117 @@
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
+let brain;
+
+let Slider;
+
+
+
+function setup() {
+  var canvas =createCanvas(640, 480);
+  canvas.position(0, 52);
+
+  Slider = createSlider(0, 100, 0);
+  Slider.position(5,520);
+  
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+
+  let options = {
+    inputs: 34,
+    outputs: 1,
+    task: 'regression',
+    debug: true
+  }
+  brain = ml5.neuralNetwork(options);
+  const modelInfo = {
+    model: 'mountainmodel.json',
+    metadata: 'mountainmodel_meta.json',
+    weights: 'mountainmodel.weights.bin',
+  };
+  brain.load(modelInfo, brainLoaded);
+}
+
+function brainLoaded() {
+  console.log('pose predicting ready!');
+  predictColor();
+}
+
+function predictColor() {
+  if (pose) {
+    let inputs = [];
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      inputs.push(x);
+      inputs.push(y);
+    }
+    brain.predict(inputs, gotResult);
+  } else {
+    setTimeout(predictColor, 100);
+  }
+}
+
+function gotResult(error, results) {
+  console.log(results);
+  let r = results[0].value;
+ 
+  Slider.value(r);
+
+  predictColor();
+}
+
+
+
+
+
+
+function gotPoses(poses) {
+  // console.log(poses); 
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+  }
+}
+
+
+function modelLoaded() {
+  console.log('poseNet ready');
+}
+
+function draw() {
+  push();
+  translate(video.width, 0);
+  scale(-1, 1);
+  image(video, 0, 0, video.width, video.height);
+  
+
+  if (pose) {
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(0);
+
+      line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0);
+      stroke(255);
+      ellipse(x, y, 16, 16);
+    }
+  }
+  pop();
+
+  let x = Slider.value();
+  fill("#c11325");
+  textSize(36);
+  text(x, 10, height - 10);
+
+}
